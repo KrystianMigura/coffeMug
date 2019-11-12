bcb7665e67e96d5fe6e6f8237ea3d4a6e4ccca1e
KrystianMigura <Krystian.migura@gmail.com>
2019-11-12 22:19:33 +0100

    simple post and get information
    to do
    create file for entity
    add vallue to file
    update
    remove
    get

---
+++ WebApplication1/Controllers/ProductController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers 
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            //ma zwracac liste produktów
+            Models.Products allEntity = new Models.Products();
+            
+             String[] information = allEntity.returnAllEntity();
+            return information;
+            //return new string[] { "value1222", "value23333" };
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            Models.Products asdf = new Models.Products();
+            string info = asdf.returnList(id);
+            return info;
+            //ma zwracac produkt o danym id
+
+        }
+
+        public class myModel
+        {
+            public string Key { get; set; }
+            public int Id { get; set; }
+            public double price { get; set; }
+        }
+
+        [HttpPost]
+        public string Post([FromBody] Models.model model )
+        {
+            //save entity + validators inside class model
+            return model.ProductCreateInputModel();
+            
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+            //aktualizuje dany produkt
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            //usuwa produkt
+        }
+    }
+}
---
+++ WebApplication1/Models/Products.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Products
+    {
+        public Products() { }
+
+        public string returnList(int param)
+        {
+            return "jakis absurd :D  "+ param ;
+        }
+
+        public string[] returnAllEntity()
+        {
+           return new string[] {"data", "1", "2", "3" };
+        }
+
+        public string blabla()
+        {
+            return "QPA";
+        }
+    }
+
+    public class model
+    {
+        public model() { }
+        public string Name { get; set; }
+        public string Id { get; set; }
+        public decimal Price { get; set; }
+
+        public string ProductCreateInputModel()
+        {
+            bool _Name = checkName(this.Name);
+            bool _Price = checkPrice(this.Price);
+            Guid guid = generateGuid();
+
+            if (_Name == true && _Price == true && guid != null)
+            {
+                string value = "Id :" + guid + " | " + "Name :" + this.Name + " | " + "Price :" + this.Price + "?";
+                return value;
+            }
+            else
+            {
+                return "400	Bad Request";
+            }
+            
+
+        }
+
+        private bool checkName(string _name)
+        {
+            return (_name != null) ? true : false;
+        }
+        private bool checkPrice(decimal _price)
+        {
+            string a = _price.ToString();    
+            int index1 = a.IndexOf(',');
+            if (index1 == -1 && _price > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        private Guid generateGuid()
+        {
+            Guid newGuid = Guid.NewGuid();
+            return newGuid;
+        }
+
+    }
+}
